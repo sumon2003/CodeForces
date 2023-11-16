b97f9f4240414ab400fd90b50b36aa664c1e3f39
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-16 16:44:32 +0600

    Add files via upload

---
+++ Team.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+int n,i,petya,vasya,tonya,count=0;
+scanf("%d",&n);
+for(i=0; i<n; i++){
+   scanf("%d%d%d",&petya,&vasya,&tonya);
+    if(petya==1 && vasya==1 && tonya==1)
+     count++;
+    if(petya==1 && vasya==1 && tonya==0)
+     count++;
+    if(petya==1 && vasya==0 && tonya==1)
+     count++;
+    if(petya==0 && vasya==1 && tonya==1)
+     count++;
+    if(petya==1 && vasya==0 && tonya==0)
+     continue;
+    if(petya==0 && vasya==1 && tonya==0)
+     continue;
+    if(petya==0 && vasya==0 && tonya==1)
+     continue;
+    if(petya==0 && vasya==0 && tonya==0)
+     continue;
+}
+printf("%d",count);
+return 0;
+}
