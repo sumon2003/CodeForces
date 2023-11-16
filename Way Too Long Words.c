2414bb8296e9d7e73d6b67b2804c567853f6bf07
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-16 16:39:31 +0600

    Add files via upload

---
+++ Way Too Long Words.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+ 
+int main() {
+   int t,i;
+   char string[101];
+   scanf("%d",&t);
+   for(i=0; i<t; i++){
+    scanf("%s",string);
+   int len = strlen(string);
+   if(len>10){
+    printf("%c%d%c\n",string[0],len-2,string[len-1]);
+   }
+   else{
+    printf("%s\n",string);
+   }
+}
+  return 0;
+}
