9627b4b052f208daffea22763e1baa2b56b0d53a
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-16 17:06:56 +0600

    Add files via upload

---
+++ Bit++.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+ 
+int main() {
+   int t,i,count=0;
+   char bit[10];
+   scanf("%d",&t);
+   for(i=0; i<t; i++){
+    scanf("%s",bit);
+    if(bit[0]=='+' || bit[2]=='+'){
+      count++;
+    }
+    else if(bit[0]=='-' || bit[2]=='-'){
+        count--;
+    }
+   }
+   printf("%d\n",count);
+  return 0;
+}
