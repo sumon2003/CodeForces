0c04f80b4d330e605d37a922e126f5a681a04b97
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-16 17:03:33 +0600

    Add files via upload

---
+++ Next Round.c
@@ -0,0 +1,26 @@
+/// Sumon Khan ///
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<limits.h>
+#include<stdbool.h>
+int main()
+{
+ int n,k,i,count=0;
+ scanf("%d %d",&n,&k);
+ int arr[n];
+ for(i=0; i<n; i++){
+  scanf("%d",&arr[i]);
+ }
+ for(i=0; i<n; i++){
+  if(arr[i]>=arr[k-1] && arr[i]!=0){
+    count++;
+  }
+  
+ }
+ printf("%d",count);
+ 
+ return 0;
+}
