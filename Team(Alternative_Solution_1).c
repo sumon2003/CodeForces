166ae83027c9042ae77ff44b23add66192c6ff8f
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-16 16:54:34 +0600

    Add files via upload

---
+++ Team(Alternative_Solution_1).c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,n,i,count=0;
+    scanf("%d",&n);
+    for(i=0; i<n; i++){
+        scanf("%d%d%d",&a,&b,&c);
+        if(a==1 && b==1 && c==1){
+            count++;
+        }
+        if(a==1 && b==1 && c==0){
+            count++;
+        }
+        if(a==1 && b==0 && c==1){
+            count++;
+        }
+        if(a==0 && b==1 && c==1){
+            count++;
+        }
+        
+    }
+    printf("%d\n",count);
+ 
+   return 0;
+ 
+}
