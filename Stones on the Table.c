2f893b54af339d155b5ffa812b09bb03ec61fac3
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-17 21:29:25 +0600

    Add files via upload

---
+++ Stones on the Table.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main() {
+  int n,i,count=0;
+  char colour[51];
+  scanf("%d",&n);
+  scanf("%s",colour);
+  for(i=0; i<n; i++)
+  {
+    if(colour[i]==colour[i+1])
+    {
+      count++;
+    }
+  }
+  printf("%d",count);
+ 
+  return 0;
+}
+ 
+ 
