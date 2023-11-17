b3651301ece443008f578c718f00d6b9450cf44d
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-17 21:25:14 +0600

    https://codeforces.com/problemset/problem/236/A
    Those days, many boys use beautiful girls' photos as avatars in forums. So it is pretty hard to tell the gender of a user at the first glance. Last year, our hero went to a forum and had a nice chat with a beauty (he thought so). After that they talked very often and eventually they became a couple in the network.
    
    But yesterday, he came to see "her" in the real world and found out "she" is actually a very strong man! Our hero is very sad and he is too tired to love again now. So he came up with a way to recognize users' genders by their user names.
    
    This is his method: if the number of distinct characters in one's user name is odd, then he is a male, otherwise she is a female. You are given the string that denotes the user name, please help our hero to determine the gender of this user by his method.
    
    Input
    The first line contains a non-empty string, that contains only lowercase English letters — the user name. This string contains at most 100 letters.
    
    Output
    If it is a female by our hero's method, print "CHAT WITH HER!" (without the quotes), otherwise, print "IGNORE HIM!" (without the quotes).
    
    Examples
    input
    wjmzbmr
    output
    CHAT WITH HER!
    input
    xiaodao
    output
    IGNORE HIM!
    input
    sevenkplus
    output
    CHAT WITH HER!
    Note
    For the first example. There are 6 distinct characters in "wjmzbmr". These characters are: "w", "j", "m", "z", "b", "r". So wjmzbmr is a female and you should print "CHAT WITH HER!".

---
+++ Boy or Girl.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+ 
+int main() {
+ char user_name[101];
+ scanf("%s",user_name);
+ int length = strlen(user_name);
+ int i,j,n=length;
+ for(i=0; i<n; i++){
+  for(j=i+1; j<n; j++){
+    if(user_name[i]==user_name[j]){
+      length--;
+      break;
+    }  
+  } 
+ }
+ if(length%2==0){
+  printf("CHAT WITH HER!\n");
+ }
+ else{
+  printf("IGNORE HIM!\n");
+ }
+  return 0;
+}
