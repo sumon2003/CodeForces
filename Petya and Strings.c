d247a134ab531d737ef7b36e881f08fb4dceb46f
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-17 19:16:44 +0600

    Add files via upload

---
+++ Petya and Strings.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+ 
+int main() {
+ 
+  char string1[101], string2[101];
+  scanf("%s %s",string1,string2);
+  int len = strlen(string1);
+  int i;
+  for(i=0; i<len; i++){
+    string1[i] = tolower(string1[i]);
+    string2[i] = tolower(string2[i]);
+  }
+  for(i=0; i<len; i++){  
+    if(string1[i]>string2[i]){
+      printf("1\n");
+      return 0;
+    }
+    else if(string1[i]<string2[i]){
+      printf("-1\n");
+      return 0;
+    } 
+  }
+  printf("0\n");
+  return 0;
+}
