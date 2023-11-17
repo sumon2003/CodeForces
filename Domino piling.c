ea838c46b93d8b95862af22662421ae90c8d4244
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-17 19:00:49 +0600

    Add files via upload

---
+++ Domino piling.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+ 
+int main() {
+   int m,n,square,max_num_dominoes;
+   scanf("%d %d",&m,&n);
+   square = m * n;
+   max_num_dominoes = square/2;
+   printf("%d",max_num_dominoes);
+   
+  return 0;
+}
