8e3e32e05fc3f0cd8105401b27da472300cb689b
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-18 18:23:18 +0600

    https://codeforces.com/problemset/problem/546/A

---
+++ Soldier and Bananas.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<ctype.h>
+int main() {
+ int k,n,w;
+ scanf("%d %d %d",&k,&n,&w);
+ int i,total_cost=0,borrowed_Money;
+ for(i=1; i<=w; i++)
+ {
+  total_cost += k*i;
+ }
+ if(total_cost<=n)
+ {
+  printf("0\n");
+ }
+ else if(total_cost>n)
+ {
+  borrowed_Money = total_cost - n;
+  printf("%d",borrowed_Money);
+ }
+ 
+  return 0;
+}
