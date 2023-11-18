539d9496460a2893834bb4e4567363c7ea8deb08
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-18 18:42:59 +0600

    https://codeforces.com/problemset/problem/96/A

---
+++ Football.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+int main(){
+char S[101];
+scanf("%s",S);
+int len_S = strlen(S);
+int i,count=1; //count=1 for the first player.There's at least one player from each team present on the field.
+for(i=0; i<len_S; i++){
+  if(S[i]==S[i+1]){
+    count++;
+    if(count == 7){
+      printf("YES");
+      return 0;
+    }
+  }else{
+    count = 1;
+  }
+}  
+printf("NO");
+  return 0;
+}
