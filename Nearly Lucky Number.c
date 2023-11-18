7e77cf74ef04d2a272e231ea2ebe2cb60f46ee39
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-18 21:04:20 +0600

    https://codeforces.com/problemset/problem/110/A

---
+++ Nearly Lucky Number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+int main(){
+long long int num;
+scanf("%lld",&num);
+char string[100];
+sprintf(string, "%lld", num);
+int len_string = strlen(string);
+int i,count=0;
+for(i=0; i<len_string; i++){
+  if(string[i]=='7' || string[i]=='4'){
+    count++;
+  }
+}
+if(count==7 || count==4){
+  printf("YES");
+}else{
+  printf("NO");
+}
+  return 0;
+}
+ 
+ 
