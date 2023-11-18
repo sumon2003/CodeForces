3a1145f77ba0e972dc41a8f05dcbc2bc8219ae5d
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-18 18:26:51 +0600

    https://codeforces.com/problemset/problem/59/A

---
+++ Word.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+int main(){
+char string[101];
+scanf("%s",string);
+int len_string = strlen(string);
+int i, count_upper = 0, count_lower = 0;
+for(i=0; i<len_string; i++){
+  if(string[i]>='A' && string[i]<='Z'){
+    count_upper++;
+  }
+  else if(string[i]>='a' && string[i]<='z'){
+    count_lower++;
+  }
+}
+//printf("%d\n",count_upper);
+//printf("%d\n",count_lower);
+for(i=0; i<len_string; i++){
+  if(count_upper>count_lower){
+    printf("%c",toupper(string[i]));
+  }
+  else if(count_upper<=count_lower){
+    printf("%c",tolower(string[i]));
+  }
+}
+ 
+  return 0;
+}
+ 
