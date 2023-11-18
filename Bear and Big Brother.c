31a25dfc987ba2a8cd84b7db8017a60f7ccc8246
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-18 18:17:16 +0600

    https://codeforces.com/problemset/problem/791/A

---
+++ Bear and Big Brother.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main() {
+  int a,b,count=0;
+  scanf("%d %d",&a,&b);
+  while(a<=b){
+    a=a+a+a;
+    b=b+b;
+    count++;
+  }
+  printf("%d",count);
+ 
+  return 0;
+}
+ 
