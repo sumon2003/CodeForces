07cd7bfa3a9f790f631b2bc846864917221592c0
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-18 18:32:37 +0600

    https://codeforces.com/problemset/problem/977/A

---
+++ Wrong Subtraction.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+int main(){
+int n,k,i;
+scanf("%d %d",&n,&k);
+for(i=0; i<k; i++){
+  int last_digit=n%10;
+  if(last_digit == 0){
+    n/=10;
+  }
+  else if(last_digit != 0){
+    n--;
+  }
+}
+printf("%d\n",n);
+ 
+  return 0;
+}
