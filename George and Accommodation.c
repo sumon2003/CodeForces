d73920724c29636c7f8d85329c446375d5e969a0
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-19 23:07:50 +0600

    https://codeforces.com/problemset/problem/467/A

---
+++ George and Accommodation.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+int main(){
+int n;
+scanf("%d",&n);
+int Pi, Qi, i, count=0;
+for(i=0; i<n; i++){
+  scanf("%d %d",&Pi,&Qi);
+  if(Qi-Pi==1){
+    continue;
+  }
+  else if(Pi<Qi){
+    count++;
+  }
+}
+printf("%d",count);
+    
+  return 0;
+}
