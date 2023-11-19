b584fad658b8458eb4b849b58367cb3f92d79e68
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-19 23:01:29 +0600

    https://codeforces.com/problemset/problem/1030/A

---
+++ In Search of an Easy Problem.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+int main(){
+int n;
+scanf("%d",&n);
+int arr[n], i;
+for(i=0; i<n; i++){
+  scanf("%d",&arr[i]);
+    if(arr[i]==1){
+      printf("HARD");
+      return 0;
+    }
+}
+printf("EASY");
+    
+  return 0;
+}
