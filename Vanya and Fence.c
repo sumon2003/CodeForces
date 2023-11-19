74e25bc6c66096c718611e3c645234f864bf9970
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-19 22:50:18 +0600

    https://codeforces.com/problemset/problem/677/A

---
+++ Vanya and Fence.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+int main(){
+int n,h;
+scanf("%d %d",&n,&h);
+int arr[n],i,count=0;
+for(i=0; i<n; i++){
+  scanf("%d",&arr[i]);
+}
+for(i=0; i<n; i++){
+  if(arr[i]<=h){
+    count++;
+  }
+  if(arr[i]>h){
+    count+=2;
+  }
+}
+printf("%d",count);
+ 
+ 
+  return 0;
+}
