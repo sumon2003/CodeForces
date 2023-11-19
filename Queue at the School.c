59603a0e33c8d87330b7439a78304ed5b2c91892
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-19 22:53:29 +0600

    https://codeforces.com/problemset/problem/266/B

---
+++ Queue at the School.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+int main(){
+int n,t;
+scanf("%d %d",&n,&t);
+char S[51],temp;
+scanf("%s",S);
+int i,j;
+for(j=0; j<t; j++){
+  for(i=0; i<n; i++){
+    if(S[i]=='B' && S[i+1]=='G'){
+      temp = S[i];
+      S[i] = S[i+1];
+      S[i+1] = temp;
+      i++;
+    }
+  }
+}
+printf("%s",S);
+ 
+ 
+  return 0;
+}
