0356ff9d184d9246afc080c4d38feeea61ad3930
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-19 22:59:41 +0600

    https://codeforces.com/problemset/problem/58/A

---
+++ Chat room.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+int main(){
+char S[101];
+scanf("%s",S);
+int len_S = strlen(S);
+char word[] = "hello";
+int j=0, count=0, i;
+for(i=0; i<len_S; i++){
+  if(S[i]==word[j]){
+    j++;
+    count++;
+  }
+}
+if(count==5){
+  printf("YES");
+}
+else{
+  printf("NO");
+}
+    
+  return 0;
+}
