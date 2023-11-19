293223f169e444b062f2f5336747f85e5bc73fe3
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-19 22:46:56 +0600

    https://codeforces.com/problemset/problem/41/A

---
+++ Translation.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+int main(){
+char s1[101],s2[101];
+scanf("%s",s1);
+scanf("%s",s2);
+strrev(s1);
+if(strcmp(s1, s2) == 0){
+  printf("YES");
+}else{
+  printf("NO");
+}
+  return 0;
+}
