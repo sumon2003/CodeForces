69dbeab1a9a17b26670a312809a99a3cb8d30ae6
sumon2003 <121485281+sumon2003@users.noreply.github.com>
2023-11-19 22:56:00 +0600

    Add files via upload

---
+++ Beautiful Year.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+#include<stdlib.h>
+int main(){
+  int year;
+  scanf("%d",&year);
+  char Year[5];
+  
+  while(1){
+    year++;
+    sprintf(Year, "%d", year);
+    int isDuplicate=0;
+     for(int i=0; i<4; i++){
+       for(int j=i+1; j<4; j++){
+         if(Year[i]==Year[j]){
+           isDuplicate=1;
+           break;
+         }
+       }
+       if(isDuplicate){
+         break;
+       }
+     }
+    if(!isDuplicate){
+      printf("%d",year);
+      break;
+    }
+  }
+ 
+ 
+  return 0;
+}
